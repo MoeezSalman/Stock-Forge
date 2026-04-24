5f3cc78fd653812dfd6130eeb6b2e2eaacc338d6
Muhammad Moeez <moeezsalman246@gmail.com>
2026-04-24 18:46:08 +0500

    corrected theme

--- StockForge/src/App.jsx
+++ StockForge/src/App.jsx
@@ -1,9 +1,19 @@
-import SentimentDashboard from './Components/SentimentAnalysisEngineScreen'
-import Dashboard from './Components/Dashboard'
-import AlphaSignalAI from './Components/AlphaSignalAI'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import SentimentDashboard from "./Components/SentimentAnalysisEngineScreen";
+import Dashboard from "./Components/Dashboard";
+import AlphaSignalAI from "./Components/AlphaSignalAI";
 
 function App() {
-  return <AlphaSignalAI />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/sentiment" element={<SentimentDashboard />} />
+        <Route path="/alpha" element={<AlphaSignalAI />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
